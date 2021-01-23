7a90ca877c6b80f7626758a605006d882fed82e3
PrianiKate <li.liliana@yandex.ru>
2021-01-23 19:38:02 +0400

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let arr = [];
+    n = Math.abs(n);
+    while (n > 0) {
+        arr.push(n % 10);
+        n = (n - n % 10) / 10;
+    }
+    let ans = 0;
+    let d = 1;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        ans += arr[i] * d;
+        d *= 10;
+    }
+    return ans;
 }
